c7019cea16620e5f7bc8bb33fafa4bc5e3488ce8
Naoki Morita <namorit@gmail.com>
2026-01-23 20:23:05 +0900

    Firebaseプロジェクトを変更

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDloISTS5y1zLKpopqctmbLzJSAdBsGdWg",
-  authDomain: "beflux-admin.firebaseapp.com",
-  projectId: "beflux-admin",
-  storageBucket: "beflux-admin.firebasestorage.app",
-  messagingSenderId: "544614902660",
-  appId: "1:544614902660:web:1027a4396a92d23ad04bd1",
-  measurementId: "G-09Z20B32LF"
+  apiKey: "AIzaSyAHXfHwf4QRBigp_HlbImcXC71qWL9EkSY",
+  authDomain: "beflux-inc.firebaseapp.com",
+  projectId: "beflux-inc",
+  storageBucket: "beflux-inc.firebasestorage.app",
+  messagingSenderId: "34957437086",
+  appId: "1:34957437086:web:2876919347e31d1e4eb38f",
+  measurementId: "G-F0T2GQV957"
 };
 
 const app = initializeApp(firebaseConfig);
